8a7bf4202b0ea175fdfe6977946ff8352c6818e1
aadig <spider.guya360@gmail.com>
2023-11-22 20:06:01 +0000

    Had to add github link

---
+++ src/main/java/net/aadi/mccourse/MCCourseMod.java
@@ -0,0 +1,17 @@
+package net.aadi.mccourse;
+
+import net.aadi.mccourse.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCourseMod implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+	}
+}
